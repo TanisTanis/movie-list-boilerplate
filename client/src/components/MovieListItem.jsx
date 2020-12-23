e1ff59c3391eb59e4f95387a1082fc6d244e80ae
Tanis Kiel <tkiel12@gmail.com>
2020-12-23 14:45:08 -0800

    All info now show when clicking a movie title, including watch button

--- client/src/components/MovieListItem.jsx
+++ client/src/components/MovieListItem.jsx
@@ -5,26 +5,40 @@ class MovieListItem extends React.Component {
     super(props);
 
     this.state = {
-      watched: false
+      watched: false,
+      info: false
     }
 
-    this.handleClick = this.handleClick.bind(this);
+    this.handleWatchClick = this.handleWatchClick.bind(this);
+    this.handleMovieNameClick = this.handleMovieNameClick.bind(this);
   }
 
-  handleClick(event) {
+  handleWatchClick(event) {
     this.setState({
       watched: !this.state.watched
     });
     this.props.switchWatched(event.target.name);
   }
 
+  handleMovieNameClick(event) {
+    this.setState({
+      info: !this.state.info
+    });
+  }
+
   render() {
     let className = this.state.watched ? "watched-movie watch" : "unwatched-movie watch"
     return (
       <div className="movie-title-div">
-        <span className="movie-title">{this.props.movie.title}</span>
-        {!this.state.watched ? <button onClick={this.handleClick} className={className} name={this.props.movie.title}>To watch</button> : null}
-        {this.state.watched ? <button onClick={this.handleClick} className={className} name={this.props.movie.title}>Watched</button> : null}
+        <span className="movie-title" onClick={this.handleMovieNameClick}>{this.props.movie.title}</span>
+        {this.state.info ? <div className="movie-info">
+          <div className="info year">Year: {this.props.movie.year}</div>
+          <div className="info runtime">Runtime: {this.props.movie.runtime}</div>
+          <div className="info metascore">Metascore: {this.props.movie.metascore}</div>
+          <div className="info imdb">Imdb Rating: {this.props.movie['imdb Rating']}</div>
+          {!this.state.watched ? <button onClick={this.handleWatchClick} className={className} name={this.props.movie.title}>To watch</button> : null}
+          {this.state.watched ? <button onClick={this.handleWatchClick} className={className} name={this.props.movie.title}>Watched</button> : null}
+        </div> : null}
       </div>
     )
   }
