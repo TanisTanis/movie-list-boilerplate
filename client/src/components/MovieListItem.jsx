18df887b642ba9aed5c38ed9d2b9bc3d1282fef6
Tanis Kiel <tkiel12@gmail.com>
2020-12-23 13:49:04 -0800

    Watched/To watch states added

--- client/src/components/MovieListItem.jsx
+++ client/src/components/MovieListItem.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
 
-let MovieListItem = (props) => (
-  <div className="movie-title-div">
-    <span className="movie-title">{props.movie.title}</span>
-  </div>
-)
+class MovieListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      watched: false
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    this.setState({
+      watched: !this.state.watched
+    });
+  }
+
+  render() {
+    let className = this.state.watched ? "watched-movie watch" : "unwatched-movie watch"
+    return (
+      <div className="movie-title-div">
+        <span className="movie-title">{this.props.movie.title}</span>
+        {!this.state.watched ? <button onClick={this.handleClick} className={className}>To watch</button> : null}
+        {this.state.watched ? <button onClick={this.handleClick} className={className}>Watched</button> : null}
+      </div>
+    )
+  }
+}
 
 export default MovieListItem;
--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ const PORT = 3000 || process.env.PORT;
 
 app.use(express.static('public'));
 
+app.use(express.json());
+
+
+
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
 })
